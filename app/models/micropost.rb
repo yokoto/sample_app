41e44c2aea02c4df06892cbb5a61b1c707eeace0
yokoto <youkotoushi@gmail.com>
2014-10-24 21:11:41 +0900

    modify tabs in micropost.rb

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,13 +4,13 @@ class Micropost < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
 
-	# 与えられたユーザーがフォローしているユーザー達のマイクロポストを返す。
+  # 与えられたユーザーがフォローしているユーザー達のマイクロポストを返す。
   # Relationship.followed_id ≒ User.id == Micropost.user_id
-	def self.from_users_followed_by(user) 
-		followed_user_ids = "SELECT followed_id FROM relationships
+  def self.from_users_followed_by(user) 
+	followed_user_ids = "SELECT followed_id FROM relationships
 							WHERE follower_id = :user_id" 
-		# Micropost.					
-		where("user_id IN (#{followed_user_ids} ) OR user_id = :user_id",
-			 user_id: user.id)  # user_id = フォローしているユーザー OR 与えられたユーザー。 ORは、２つの条件のどちらかを満たすものであれば結果として返す。
-	end
+	# Micropost.					
+	where("user_id IN (#{followed_user_ids} ) OR user_id = :user_id",
+		 user_id: user.id)  # user_id = フォローしているユーザー OR 与えられたユーザー。 ORは、２つの条件のどちらかを満たすものであれば結果として返す。
+  end
 end
