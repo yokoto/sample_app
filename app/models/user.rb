79f4d23d1e0653af3f635c2cf9a9902b22c44e22
yokoto <youkotoushi@gmail.com>
2014-12-01 17:16:58 +0900

    add comment about selfjoin to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy  # 2つのモデルの間の「第3のモデル」(結合モデル)の介在
-  has_many :followed_users, through: :relationships, source: :followed
+  has_many :followed_users, through: :relationships, source: :followed  # 自己結合
+  # belongs_to :relationships
 
   has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy  # has_many :relationshipsの逆
-  has_many :followers, through: :reverse_relationships#, source: :follower
+  has_many :followers, through: :reverse_relationships#, source: :follower  # 自己結合
+  # belongs_to :reverse_relationships
+
   has_secure_password
   before_save { email.downcase! }
   # User生成のタイミングでコールバック
