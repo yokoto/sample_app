6df7bc5e5031aebdc9d1210cfad6a8ddbaab791f
yokoto <youkotoushi@gmail.com>
2014-12-30 05:38:55 +0900

    add REST API

---
+++ app/api/api.rb
@@ -0,0 +1,25 @@
+class API < Grape::API
+  format :json
+  prefix :api
+
+  resource "users" do
+	desc "returns all users"
+	get do
+	  User.all
+	end
+
+  resource "microposts" do
+	desc "result all microposts"
+	get do
+	  Micropost.all
+	end
+  end
+
+  resource "relationships" do
+	desc "result all relationships"
+	get do
+	  Relationship.all
+	end
+  end
+end
+
